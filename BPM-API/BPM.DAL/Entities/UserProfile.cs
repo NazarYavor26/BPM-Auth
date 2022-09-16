8a0bb2db5d1fa5eda8463409bf755444110b1998
NazarYavor26 <92759124+NazarYavor26@users.noreply.github.com>
2022-09-16 13:39:34 +0300

    fixed relation in database models

--- BPM-API/BPM.DAL/Entities/UserProfile.cs
+++ BPM-API/BPM.DAL/Entities/UserProfile.cs
@@ -12,9 +12,6 @@ namespace BPM.DAL.Entities
         public int Id { get; set; }
 
         [Required]
-        public int UserId { get; set; }
-
-        [Required]
         [MaxLength(150)]
         public string FirstName { get; set; }
 
@@ -22,21 +19,20 @@ namespace BPM.DAL.Entities
         [MaxLength(150)]
         public string LastName { get; set; }
 
-        public int LevelId { get; set; }
-
         [Required]
-        public DateTime LastUpdated { get; set; }
-
-        [Required]
-
-        public int? CompetenceLeadId { get; set; }
+        public int UserId { get; set; }
 
         public User User { get; set; }
 
+        [Required]
+        public int LevelId { get; set; }
+
         public virtual Level Level { get; set; }
 
-        //public virtual User LastUpdatedBy { get; set; }
+        [Required]
+        public virtual User LastUpdatedBy { get; set; }
 
-        //public virtual User CompetenceLeadId { get; set; }
+        [Required]
+        public virtual User CompetenceLeadId { get; set; }
     }
 }
