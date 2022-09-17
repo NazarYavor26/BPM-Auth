b2f99a19b8c89ac6f303571ff235a894172ae849
NazarYavor26 <92759124+NazarYavor26@users.noreply.github.com>
2022-09-17 22:13:31 +0300

    added password property

--- BPM-API/BPM.DAL/Entities/User.cs
+++ BPM-API/BPM.DAL/Entities/User.cs
@@ -15,6 +15,8 @@ namespace BPM.DAL.Entities
         [MaxLength(150)]
         public string Email { get; set; }
 
+        public string? Password { get; set; }
+
         [Required]
         public Enums.Rore RoleId { get; set; }
 
